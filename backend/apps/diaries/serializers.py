2d4fca9b021548a5ad35c17919a1fb704412041a
ps_belavin_a <belavin@place-start.ru>
2025-10-15 08:39:27 +0300

    create base diaries app

--- backend/apps/diaries/serializers.py
+++ backend/apps/diaries/serializers.py
@@ -2,8 +2,33 @@ from rest_framework import serializers
 from . import models
 
 
-# class DiarySerializer(serializers.ModelSerializer):
+class DiaryListSerializer(serializers.ModelSerializer):
+    in_wishlist = serializers.SerializerMethodField()
 
-#     class Meta:
-#         model = models.Diary
-#         fields = '__all__'
+    class Meta:
+        model = models.Diary
+        fields = (
+            'id',
+            'creator',
+            'title',
+            'short_description',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wishlist_ids = self.context['request'].user.get_diary_wishlist().get_ids()
+
+    def get_in_wishlist(self, instance):
+        return instance.pk in self.wishlist_ids
+
+
+class DiarySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Diary
+        exclude = (
+            'created_by_diocese',
+            'creator',
+            'updated_at',
+            'created_at',
+        )
